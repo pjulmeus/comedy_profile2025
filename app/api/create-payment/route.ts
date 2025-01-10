3aac595598681112464dad8a02df4930f19ffff3
pjulmeus <pjulmeus@gmail.com>
2025-01-10 12:48:35 -0500

    reverted api version

--- app/api/create-payment/route.ts
+++ app/api/create-payment/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2022-11-15',
+  apiVersion: '2024-12-18.acacia',
 });
 
 export async function POST(request: Request) {
